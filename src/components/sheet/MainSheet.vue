eb26a84ff8b19ac11fb4b483013fe9da2fef9cc4
cxey-low <1624202438@qq.com>
2023-12-23 18:02:06 +0800

    tried to make a sheet

--- src/components/sheet/MainSheet.vue
+++ src/components/sheet/MainSheet.vue
@@ -2,11 +2,45 @@
 
 let a:string = '123';
 
+let sb = {
+    head_lenth:10,
+    heads:[
+        {row:0,col:0,text:''}
+    ],
+    row_lenth:10,
+    col_lenth:10,
+    rows:[
+        [{row:1,col:0,text:''}]
+    ]
+}
+
+{for(let i:number = sb.heads.length;i<sb.head_lenth;i++){
+    sb.heads[sb.heads.length] = {row:0,col:i,text:'head'}
+}
+
+function colBuilder() {
+    
+    for(let i:number = 0;i<sb.col_lenth;i++){
+
+    }
+}
+
+for(let i:number = sb.rows.length;i<=sb.row_lenth;i++){
+    sb.rows[sb.rows.length] = {}
+}
+}
 </script>
 <template>
 
     <div id="sheet">
         <h1>{{ a }}</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th v-for="cell in sb.heads" v-text="cell.text"></th>
+                </tr>
+            </thead>
+        </table>
     </div>
 
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
 import Sheet from './components/sheet/MainSheet.vue'
 
-createApp(Sheet).mount('#sheet')
-createApp(App).mount('#app')
+createApp(Sheet).mount('#app')
